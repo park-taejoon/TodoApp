320067000bed2e52a85d09c4ecbb2788097536c9
pyh1987 <pyh1987@naver.com>
2022-10-18 01:27:06 +0900

    init

--- App.js
+++ App.js
@@ -1,33 +1,19 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import DateHead from './components/DateHead';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
 function App() {
+  const today = new Date();
+  console.log(today);
   return (
-    <SafeAreaView>
-      <View>
-        <Text>TodoApp</Text>
-      </View>
-    </SafeAreaView>
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView>
+          <DateHead date={today} />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
